e3d455eff7bd2a429acd34400d93c2c975428751
Koushik Ghosh <koushik.ghosh@selise.ch>
2021-08-14 01:42:51 -0700

    Implemented Interfaces and calsses for Domain.Core

---
+++ MicroRabit/MicroRabit.Domain.Core/Bus/IEventBus.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using MicroRabit.Domain.Core.Commands;
+using MicroRabit.Domain.Core.Events;
+using System.Threading.Tasks;
+
+namespace MicroRabit.Domain.Core.Bus
+{
+   public interface IEventBus
+    {
+        Task SendCommand<T>(T command) where T : Command;
+        void Puslish<T>(T @event) where T : Event;
+        Task Subscribe<T, TH>()
+            where T : Event
+            where TH : IEventHandler<T>;
+    }
+}
