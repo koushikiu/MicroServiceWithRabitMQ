cbed468c6ae8ef440dc32e5b580f5338ff52466c
Koushik Ghosh <koushik.ghosh@selise.ch>
2021-08-16 01:01:28 -0700

    Implement models and Interfaces at MicroRabit.Banking.Domain

---
+++ MicroRabit/MicroRabit.Banking.Domain/Models/Account.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabit.Banking.Domain.Models
+{
+   public class Account
+    {
+        int AccountId { get; set; }
+        string AccountType { get; set; }
+        decimal AccountBalance { get; set; }
+    }
+}
