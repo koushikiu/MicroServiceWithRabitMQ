292f772d0039382202409f9477a965b26bbcf758
Koushik Ghosh <koushik.ghosh@selise.ch>
2021-08-16 01:33:26 -0700

    Implement DbContext and Repository a MicroRabir.Banking.Data

---
+++ MicroRabit/MicroRabit.Banking.Data/Repositpry/AccountRepository.cs
@@ -0,0 +1,27 @@
+﻿using MicroRabit.Banking.Data.Context;
+using MicroRabit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabit.Banking.Data.Repositpry
+{
+    public class AccountRepository : IAccountRepository
+    {
+        BankingDbContext _bankingDbContext;
+
+        public AccountRepository(BankingDbContext bankingDbContext)
+        {
+            _bankingDbContext = bankingDbContext;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _bankingDbContext.Accounts;
+        }
+    }
+}
+public interface IAccountRepository
+{
+    IEnumerable<Account> GetAccounts();
+}
