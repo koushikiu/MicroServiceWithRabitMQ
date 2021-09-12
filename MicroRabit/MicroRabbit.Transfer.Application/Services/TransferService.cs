badd608af24fd20e8ec0c76cadf5825cf99f1fb8
Koushik Ghosh <koushik.ghosh@selise.ch>
2021-09-12 00:04:37 -0700

    Implement Transferservice

---
+++ MicroRabit/MicroRabbit.Transfer.Application/Services/TransferService.cs
@@ -0,0 +1,28 @@
+﻿using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.Models;
+using MicroRabit.Domain.Core.Bus;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Transfer.Application.Services
+{
+    public class TransferService : ITransferService
+    {
+        private readonly ITransferRepository _transferRepository;
+        private readonly IEventBus _bus;
+        public TransferService(ITransferRepository transferRepository, IEventBus bus)
+        {
+            _transferRepository = transferRepository;
+            _bus = bus;
+        }
+        public IEnumerable<TransferLog> GetTransferLogs()
+        {
+            return _transferRepository.GetTrnasferLogs();
+        }
+    }
+}
+public interface ITransferService
+{
+    IEnumerable<TransferLog> GetTransferLogs();
+}
---
+++ MicroRabit/MicroRabbit.Transfer.Data/Repository/TrnasferRepository.cs
@@ -0,0 +1,26 @@
+﻿using MicroRabbit.Transfer.Data.Context;
+using MicroRabbit.Transfer.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Transfer.Data.Repository
+{
+    public class TrnasferRepository : ITransferRepository
+    {
+        private readonly TransferDbContext _transferDbContext;
+        public TrnasferRepository(TransferDbContext transferDbContext)
+        {
+            _transferDbContext = transferDbContext;
+        }
+        public IEnumerable<TransferLog> GetTrnasferLogs()
+        {
+            return _transferDbContext.transferLogs;
+        }
+    }
+}
+
+public interface ITransferRepository
+{
+    IEnumerable<TransferLog> GetTrnasferLogs();
+}
